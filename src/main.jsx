5967fe9ac393cb4eadf0ae67536f52fec4e4887c
Ashish Shrestha <ashish11stha.codewing@gmail.com>
2023-06-29 17:53:21 +0545

    added exercise for nisha didi

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
-
 
 ReactDOM.createRoot(document.querySelector('.root')).render(
   <>
-    <App />
+    <h1>Tasks:</h1>
+    <ol>
+      <li>Make a header component and render it in App</li>
+      <li>Make a body component and render it in App</li>
+      <li>Make a card component and render it inside the body component</li>
+      <li>pass the names of the theme team members as an array in the card component</li>
+
+    </ol>
   </>
 )
 
